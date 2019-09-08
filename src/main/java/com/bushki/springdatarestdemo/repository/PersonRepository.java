4907b3ed26131984a266a05dd00ee496b38e0cca
Luis Melgar <luis.p.melgar@disney.com>
2019-09-08 13:39:17 -0700

    initial commit

---
+++ src/main/java/com/bushki/springdatarestdemo/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.bushki.springdatarestdemo.repository;
+
+import java.util.List;
+
+import com.bushki.springdatarestdemo.entity.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
